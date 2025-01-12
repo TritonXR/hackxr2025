ebd1078fa40d89b262084e76def90950d7b7037b
John Zhou <itszhoudoh@gmail.com>
2025-01-12 02:53:41 -0800

    Marquee placeholders created

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,17 @@ import Info from '@/app/sections/Info';
 import About from '@/app/sections/About';
 import Sponsors from '@/app/sections/Sponsors';
 import FAQ from '@/app/sections/FAQ';
+import MarqueeText from '@/components/MarqueeText';
 
 export default function Home() {
     return (
         <main>
             <Info />
+            <MarqueeText text="PLACEHOLDER" direction='right'/>
             <About />
+            <MarqueeText text="PLACEHOLDER" direction='left'/>
             <Sponsors />
+            <MarqueeText text="PLACEHOLDER" direction='right'/>
             <FAQ />
         </main>
     );
